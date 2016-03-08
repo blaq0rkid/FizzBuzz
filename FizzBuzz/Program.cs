19ebb3a95468327058cb0fc468fc6945810bd59d
Kelley Crowell <1kelley.crowell@gmail.com>
2016-03-07 21:14:03 -0800

    Competed program

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 0; index < 100; index++)
+            {
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    WriteLine("FizzBuzz");
+                }
+                else if
+                    (index % 3 == 0)
+                {
+                    WriteLine("Fizz");
+                }
+                else if
+                    (index % 5 == 0)
+                {
+                    WriteLine("Buzz");
+                }
+                else
+                {   
+                    WriteLine(index);
+                }
+                    ReadKey();
+              }
+                
+          }
+    }
+}
